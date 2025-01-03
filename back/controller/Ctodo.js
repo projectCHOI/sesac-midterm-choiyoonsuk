4e01947fccfb900d6b91f37841d9aa272755cd6f
projectCHOI <github.com/projectCHOI>
2025-01-03 14:27:41 +0900

    back

---
+++ back/controller/Ctodo.js
@@ -0,0 +1,16 @@
+const { Todo } = require("../models/index");
+
+/* Todos 전체 목록 불러오기 */
+exports.readAll = async (req, res) => {};
+
+/* Todo 한 개 불러오기 */
+exports.readOne = async (req, res) => {};
+
+/* 새로운 Todo 생성 */
+exports.create = async (req, res) => {};
+
+/* 기존 Todo 수정 */
+exports.update = async (req, res) => {};
+
+/* 기존 Todo 삭제 */
+exports.delete = async (req, res) => {};
